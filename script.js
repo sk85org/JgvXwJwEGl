0466b1007ce277c497d981dc2e99735497ac6120
sk85org <52631223+sk85org@users.noreply.github.com>
2024-07-08 22:28:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-const { chromium } = require('playwright'); // Playwrightモジュールをインポート
+const { webkit, devices } = require('playwright');
 
 (async () => {
-  // Chromiumブラウザインスタンスを起動
-  const browser = await chromium.launch();
-  // 新しいブラウザコンテキストを作成
-  const context = await browser.newContext();
-  // 新しいページを開く
+  const browser = await webkit.launch(); // or 'firefox','chromium'
+  const url = 'https://ssnb.x.moneyforward.com/users/sign_in'
+  const context = await browser.newContext({
+    
+    locale: 'ja'
+  });
   const page = await context.newPage();
-  // Googleのホームページにアクセス
-  await page.goto('https://www.google.com');
-  // スクリーンショットを保存
-  await page.screenshot({ path: 'google_screenshot.png' });
-  // ブラウザを閉じる
+  await page.goto(url);
+  
+  // Enter password
+  
+  await page.fill('#sign_in_session_service_email', process.env.EMAIL);
+  await page.fill('#sign_in_session_service_password', process.env.PASSWORD);
+  // Click the button
+  await page.click('#login-btn-sumit');
+  //await page.waitForTimeout(5000);
+  await page.locator('.not-now').first().click();
+  await page.locator('a:has-text("口座")').click();
+  //page.locator('.ga-refresh-account-button').click();
+
+  await page.waitForSelector('.ga-refresh-account-button')
+  const buttons = page.locator('.ga-refresh-account-button');
+  const buttonCount = await buttons.count();
+  console.log(buttonCount);
+  
+
+  for (let i = 0; i < buttonCount; i++) {
+    await buttons.nth(i).click();
+  }
+
+  
+  //await page.screenshot({ path: 'example.png' });
   await browser.close();
-})();
+})().catch(error => { console.log(error) });
+
