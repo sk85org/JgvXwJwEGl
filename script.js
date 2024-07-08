68972baeb2ae5abb57596e6f13b460495637d187
sk85org <52631223+sk85org@users.noreply.github.com>
2024-07-08 22:41:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,29 +10,8 @@ const { chromium } = require('playwright');
   const page = await context.newPage();
   await page.goto(url);
   
-  // Enter password
-  
-  await page.fill('#sign_in_session_service_email', process.env.EMAIL);
-  await page.fill('#sign_in_session_service_password', process.env.PASSWORD);
-  // Click the button
-  await page.click('#login-btn-sumit');
-  //await page.waitForTimeout(5000);
-  await page.locator('.not-now').first().click();
-  await page.locator('a:has-text("口座")').click();
-  //page.locator('.ga-refresh-account-button').click();
 
-  await page.waitForSelector('.ga-refresh-account-button')
-  const buttons = page.locator('.ga-refresh-account-button');
-  const buttonCount = await buttons.count();
-  console.log(buttonCount);
-  
-
-  for (let i = 0; i < buttonCount; i++) {
-    await buttons.nth(i).click();
-  }
-
-  
-  //await page.screenshot({ path: 'example.png' });
+  await page.screenshot({ path: 'example.png' });
   await browser.close();
 })().catch(error => { console.log(error) });
 
