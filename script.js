48b52ca9c70989549c21fd29d65cd0e19e00de7f
sk85org <52631223+sk85org@users.noreply.github.com>
2024-07-09 23:53:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,21 +13,6 @@ const { webkit, devices } = require('playwright');
   await page.fill('#sign_in_session_service_email', process.env.EMAIL);
   await page.fill('#sign_in_session_service_password', process.env.PASSWORD);
   await page.screenshot({ path: 'example.png' });
-  // Click the button
-  await page.click('#login-btn-sumit');
-  //await page.waitForTimeout(5000);
-  await page.locator('.not-now').first().click();
-  await page.locator('a:has-text("口座")').click();
-  //page.locator('.ga-refresh-account-button').click();
-
-  await page.waitForSelector('.ga-refresh-account-button')
-  const buttons = page.locator('.ga-refresh-account-button');
-  const buttonCount = await buttons.count();
-
-  for (let i = 0; i < buttonCount; i++) {
-    await buttons.nth(i).click();
-  }
-
   
   
   await browser.close();
