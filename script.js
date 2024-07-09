b7d367d677984b29282c365fb568c54891d7f5e7
sk85org <52631223+sk85org@users.noreply.github.com>
2024-07-09 22:49:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ const { chromium, devices } = require('playwright');
 
 (async () => {
   const browser = await chromium.launch(); // or 'firefox','chromium'
-  const url = 'https://ssnb.x.moneyforward.com/'
+  const url = 'https://www.ugtop.com/spill.shtml'
   const context = await browser.newContext({
     
     locale: 'ja'
   });
   const page = await context.newPage();
   await page.goto(url);
-  
   
   await page.screenshot({ path: 'example.png' });
   await browser.close();
